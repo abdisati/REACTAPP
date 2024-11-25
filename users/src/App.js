fd133b8e615b016a4454fbbf1df4a0e0ffa8997e
Abdi Deresa <Abdishegute@gmail.com>
2024-11-24 16:47:42 -0800

    files

---
+++ users/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import UserForm from "./UserForm";
+import UserList from "./UserList";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
+  return (
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr>
+        <UserList users={users} />
+      </hr>
+    </div>
+  );
+}
+
+export default App;
